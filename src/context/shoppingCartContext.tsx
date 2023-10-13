3ccf4523168a2b2e9eaa37129dd6656c6f2bb720
Jackie <jackie15910@gmail.com>
2023-10-13 14:02:29 -0400

    Added functions, added dependencies

--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type ShoppingCartProviderProps = {
   children: ReactNode
@@ -46,8 +46,30 @@ ShoppingCartProviderProps) {
     })
   }
 
+  function decreaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id)?.quantity == 1) {
+        return currItems.filter(item => item.id !== id)
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+          return { ...item, quantity: item.quantity - 1 }
+        } else {
+          return item
+        }
+        })
+      }
+    })
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id)
+    })
+  }
+
   return (
-  <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity }}>
+  <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
     {children}
   </ShoppingCartContext.Provider>
   )
